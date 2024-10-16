1380404947399031700d12b2d135478d52def459
F-societyturkey <107001655+F-societyturkey@users.noreply.github.com>
2024-10-16 20:40:53 +0300

    kulanicibilgisi.py
    yanlış degisken adı düzeltildi.

--- autopp/kulanicibilgisi.py
+++ autopp/kulanicibilgisi.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
 
-Username = "yavruBARBAR"
-Password = "yavrubarbarsaplar777"
-deneme = "yavru barbar saplar"
+Username = "kullanıcı adın"
+Password = "sifren"
+deneme = "bio içeriği"
